94021fe2aedf77a54cbd4fee615428b11ee2ea69
ikbal arslan <ikbalarslangamedesigner@gmail.com>
2023-03-18 22:48:32 +0300

    Created basic grid

---
+++ game.js
@@ -0,0 +1,9 @@
+
+
+let container=document.querySelector('.container');
+
+for (let i = 0; i < 256; i++) {
+    let div = document.createElement('div');
+    div.classList.add('square');
+    container.appendChild(div);
+  }
