9ab1beaf6417c99f4e795b1c867f0bcf04182f23
ikbal arslan <ikbalarslangamedesigner@gmail.com>
2023-03-19 00:52:00 +0300

    Final part done i fixed the bugs.

--- game.js
+++ game.js
@@ -3,18 +3,27 @@ let gridSize = 16;
 const container = document.querySelector('.container');
 
 function createGrid() {
+  // Remove all existing squares
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+  
+  // Create new squares with the specified grid size
   for (let i = 0; i < Math.pow(gridSize, 2); i++) {
     let div = document.createElement('div');
     div.classList.add('square');
     container.appendChild(div);
   }
   
+  // Set the size of each square based on the grid size
   let squares = document.querySelectorAll('.square');
+  container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
   squares.forEach((element) => {
     element.style.height = `${512/gridSize}px`;
     element.style.width = `${512/gridSize}px`;
   });
 
+  // Add event listener to change color on mouse move
   squares.forEach((element) => {
     element.addEventListener('mousemove', () => {
       element.style.backgroundColor = 'gray';
@@ -26,8 +35,6 @@ createGrid();
 
 newGridButton.addEventListener('click', () => {
   gridSize = prompt('Enter the number of squares per side for the new grid:');
-  while (container.firstChild) {
-    container.removeChild(container.firstChild);
-  }
+  if(gridSize>100) gridSize=16; // Reset gridSize if input value is greater than 100
   createGrid();
 });
