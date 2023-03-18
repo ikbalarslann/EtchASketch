4c643cff80b9ab9b02b233185392cfa6d6779859
ikbal arslan <ikbalarslangamedesigner@gmail.com>
2023-03-19 00:11:48 +0300

    Create squares by prompt information

--- game.js
+++ game.js
@@ -1,9 +1,33 @@
+const newGridButton = document.querySelector('#grid');
+let gridSize = 16;
+const container = document.querySelector('.container');
 
-
-let container=document.querySelector('.container');
-
-for (let i = 0; i < 256; i++) {
+function createGrid() {
+  for (let i = 0; i < Math.pow(gridSize, 2); i++) {
     let div = document.createElement('div');
     div.classList.add('square');
     container.appendChild(div);
   }
+  
+  let squares = document.querySelectorAll('.square');
+  squares.forEach((element) => {
+    element.style.height = `${512/gridSize}px`;
+    element.style.width = `${512/gridSize}px`;
+  });
+
+  squares.forEach((element) => {
+    element.addEventListener('mousemove', () => {
+      element.style.backgroundColor = 'gray';
+    });
+  });
+}
+
+createGrid();
+
+newGridButton.addEventListener('click', () => {
+  gridSize = prompt('Enter the number of squares per side for the new grid:');
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+  createGrid();
+});
